610321ce7476fbf4dfc24a0aa9d43d0481c38ea6
Craixzo <50031530+Craixzo@users.noreply.github.com>
2025-09-10 17:13:36 -0400

    Update Command.ts

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -1,5 +1,7 @@
 import {
-    ApplicationCommandOptionData, ApplicationCommandOptionType, ApplicationCommandType,
+    ApplicationCommandOptionData,
+    ApplicationCommandOptionType,
+    ApplicationCommandType,
 } from 'discord.js';
 import {
     CommandConfig,
@@ -9,13 +11,13 @@ import {
 } from './types';
 import { CommandContext } from './addons/CommandAddons';
 
-const commandTypeMappings = {
+const commandTypeMappings: Record<CommandType, ApplicationCommandType> = {
     ChatInput: ApplicationCommandType.ChatInput,
     Message: ApplicationCommandType.Message,
-    User: ApplicationCommandType.User
-}
+    User: ApplicationCommandType.User,
+};
 
-const argumentTypeMappings = {
+const argumentTypeMappings: Record<string, ApplicationCommandOptionType> = {
     Subcommand: ApplicationCommandOptionType.Subcommand,
     SubcommandGroup: ApplicationCommandOptionType.SubcommandGroup,
     String: ApplicationCommandOptionType.String,
@@ -27,31 +29,27 @@ const argumentTypeMappings = {
     DiscordChannel: ApplicationCommandOptionType.Channel,
     DiscordMentionable: ApplicationCommandOptionType.Mentionable,
     SecondaryGroup: ApplicationCommandOptionType.String,
-}
+};
 
-const mapArgument = (arg: CommandArgument) => {
-    // @ts-ignore
-    const apiArgument: ApplicationCommandOptionData = {
-        name: arg.trigger,
-        description: arg.description || 'No description provided.',
-        type: argumentTypeMappings[arg.type],
-        autocomplete: arg.autocomplete || false,
-        required: arg.required !== null && arg.required !== undefined ? arg.required : true,
-        choices: arg.choices || [],
-        options: arg.args ? arg.args.map(mapArgument) : [],
-        channelTypes: arg.channelTypes,
-    }
-    return apiArgument;
-}
+const mapArgument = (arg: CommandArgument): ApplicationCommandOptionData => ({
+    name: arg.trigger,
+    description: arg.description || 'No description provided.',
+    type: argumentTypeMappings[arg.type],
+    autocomplete: arg.autocomplete || false,
+    required: arg.required ?? true,
+    choices: arg.choices || [],
+    options: arg.args ? arg.args.map(mapArgument) : [],
+    channelTypes: arg.channelTypes,
+});
 
 abstract class Command {
     trigger: string;
-    type?: CommandType;
-    description?: string;
-    module?: string;
-    aliases?: string[];
-    permissions?: CommandPermission[];
-    args?: CommandArgument[];
+    type: CommandType;
+    description: string;
+    module: string;
+    aliases: string[];
+    permissions: CommandPermission[];
+    args: CommandArgument[];
 
     constructor(options: CommandConfig) {
         this.trigger = options.trigger;
@@ -63,32 +61,19 @@ abstract class Command {
         this.args = options.args || [];
     }
 
-    /**
-     * Generate a command object for slash commands.
-     */
     generateAPICommand() {
-        if(this.type.startsWith('Subcommand')) {
-            return {
-                name: this.trigger,
-                description: this.description,
-                type: commandTypeMappings[this.type],
-                options: this.args ? this.args.map(mapArgument) : [],
-            }
-        } else {
-            return {
-                name: this.trigger,
-                description: this.description,
-                type: commandTypeMappings[this.type],
-                options: this.args ? this.args.map(mapArgument) : [],
-                defaultPermission: true,
-            }
-        }
+        return {
+            name: this.trigger,
+            description: this.description,
+            type: commandTypeMappings[this.type],
+            options: this.args.map(mapArgument),
+            defaultPermission: true,
+        };
     }
 
     /**
-     * The function to run the command.
-     * @param ctx The context of the command.
-     * @param args The arguments passed to the command, as an object mapped by ID.
+     * Function executed when the command is run.
+     * @param ctx The command context.
      */
     abstract run(ctx: CommandContext): void;
 }
