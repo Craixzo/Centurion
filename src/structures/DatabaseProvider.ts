2af485e669dfdfd7f6a314d0c348a6db1e5f15cb
Craixzo <50031530+Craixzo@users.noreply.github.com>
2026-09-07 19:02:36 -0400

    Refactor DatabaseProvider methods for better parameters

--- src/structures/DatabaseProvider.ts
+++ src/structures/DatabaseProvider.ts
@@ -1,8 +1,8 @@
 abstract class DatabaseProvider {
-    abstract findUser(query: any): Promise<any>;
-    abstract findSuspendedUsers(): Promise<any[]>;
-    abstract findBannedUsers(): Promise<any[]>;
-    abstract updateUser(query: any, data: any): Promise<void>;
+    abstract findUser(robloxId: string, groupId: number | string): Promise<any>;
+    abstract findSuspendedUsers(groupId?: number | string): Promise<any[]>;
+    abstract findBannedUsers(groupId?: number | string): Promise<any[]>;
+    abstract updateUser(robloxId: string, groupId: number | string, data: any): Promise<void>;
 }
 
 export { DatabaseProvider };
