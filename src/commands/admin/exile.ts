5ac9b9c1b0bf550b3c486a84870b004b6030c64d
Craixzo <50031530+Craixzo@users.noreply.github.com>
2026-09-07 19:16:59 -0400

    Refactor exile command to use resolveGroup

--- src/commands/admin/exile.ts
+++ src/commands/admin/exile.ts
@@ -1,6 +1,7 @@
-import { discordClient, robloxClient, robloxGroup as defaultRobloxGroup } from '../../main';
+import { discordClient, robloxClient } from '../../main';
 import { CommandContext } from '../../structures/addons/CommandAddons';
 import { Command } from '../../structures/Command';
+import { resolveGroup } from '../../handlers/groupResolver';
 import {
     getInvalidRobloxUserEmbed,
     getRobloxUserIsNotMemberEmbed,
@@ -58,11 +59,11 @@ class ExileCommand extends Command {
     }
 
     async run(ctx: CommandContext) {
-        let robloxGroup: Group = defaultRobloxGroup;
-        if(ctx.args['group']) {
-            const secondaryGroup = config.secondaryGroups.find((group) => group.name.toLowerCase() === ctx.args['group'].toLowerCase());
-            if(!secondaryGroup) return ctx.reply({ embeds: [ getInvalidRobloxGroupEmbed() ]});
-            robloxGroup = await robloxClient.getGroup(secondaryGroup.id);
+        let robloxGroup: Group;
+        try {
+            robloxGroup = await resolveGroup(ctx.guild?.id, ctx.args['group']);
+        } catch (err) {
+            return ctx.reply({ embeds: [ getInvalidRobloxGroupEmbed() ]});
         }
 
         let robloxUser: User | PartialUser;
@@ -99,8 +100,8 @@ class ExileCommand extends Command {
             if(!actionEligibility) return ctx.reply({ embeds: [ getVerificationChecksFailedEmbed() ] });
         }
 
-        const userData = await provider.findUser(robloxUser.id.toString());
-        if(userData.xp !== 0) return provider.updateUser(robloxUser.id.toString(), { xp: 0 });
+        const userData = await provider.findUser(robloxUser.id.toString(), robloxGroup.id);
+        if(userData.xp !== 0) return provider.updateUser(robloxUser.id.toString(), robloxGroup.id, { xp: 0 });
         if(userData.suspendedUntil) return ctx.reply({ embeds: [ getUserSuspendedEmbed() ] });
 
         try {
