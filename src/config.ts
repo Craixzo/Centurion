2741332d11496b5133982be04e00cafd020a7912
Craixzo <50031530+Craixzo@users.noreply.github.com>
2026-09-10 21:57:15 -0400

    Rename event types for consistency

--- src/config.ts
+++ src/config.ts
@@ -42,53 +42,66 @@ export const config: BotConfig = {
     timezone: 'America/New_York',
 
     eventTypes: {
-    patrol: [
+    patrols: [
         { name: 'Army Patrol', value: 'Army Patrol', description: 'Patrol the inside of the fort with Military Police.' },
         { name: 'Navy Patrol', value: 'Navy Patrol', description: 'Patrol the oceans around the globe.' },
         { name: 'Air Force Patrol', value: 'Air Force Patrol', description: 'Patrol the skies far above Yellonia.' },
         { name: 'Coast Guard Patrol', value: 'Coast Guard Patrol', description: 'Coming soon.' },
         { name: 'Military Police Patrol', value: 'Military Police Patrol', description: 'Patrol the inside and outside of the military facilities.' },
     ],
-    tryout: [
-        { name: 'Army Tryout', value: 'Army Tryout', description: 'Try out for the Army.' },
+    tryouts: [
+        { name: 'Army Tryout', value: 'Army Tryout', description: 'Try out for the Army. Use this for Infantry stuff as well.' },
         { name: 'Navy Tryout', value: 'Navy Tryout', description: 'Try out for the Navy.' },
         { name: 'Air Force Tryout', value: 'Air Force Tryout', description: 'Try out for the Air Force.' },
         { name: 'Military Police Tryout', value: 'Military Police Tryout', description: 'Try out for Military Police.' },
         { name: 'Marine Tryout', value: 'Marine Tryout', description: 'Try out for the Marines.' },
         { name: 'Fleet Tryout', value: 'Fleet Tryout', description: 'Try out for the Fleet.' },
-        { name: 'Armored Tryout', value: 'Armored Tryout', description: 'Try out for Armored.' },
+        { name: 'Armored Tryout', value: 'Armored Tryout', description: 'Try out for 3rd Armored Division.' },
         { name: 'Helicopter Ops Tryout', value: 'Helicopter Ops Tryout', description: 'Try out for Helicopter Operations Division.' },
         { name: 'Air Combat Tryout', value: 'Air Combat Tryout', description: 'Try out for Air Combat Division.' },
         { name: 'Ceremonial Guard Tryout', value: 'Ceremonial Guard Tryout', description: 'Try out for Ceremonial Guard.' },
     ],
-    training: [
+    trainings: [
         { name: 'Promotional Training', value: 'Promotional Training', description: 'For enlistees to rank up in the military.' },
         { name: 'Combat Training', value: 'Combat Training', description: 'Train for combat.' },
-        { name: 'FTX', value: 'FTX', description: 'Field Training Exercise.' },
+        { name: 'Joint Training', value: 'Branch Scrimmage', description: 'Joint training with an ally of Yellonia. Ask MOFA to find out who our allies are. Try your best to host them on Scarsen.' },
     ],
-    scrimmage: [
+    scrimmages: [
         { name: 'Division Scrimmage', value: 'Division Scrimmage', description: 'Battle the division you hate the most.' },
         { name: 'Branch Scrimmage', value: 'Branch Scrimmage', description: 'Battle the branch you hate the most.' },
+        { name: 'Joint Scrimmage', value: 'Joint Scrimmage', description: 'Friendly battles against allies. Ask MOFA to find out who our allies are.' },
     ],
-    operation: [
+    operations: [
         { name: 'Milsim Operation', value: 'Milsim Operation', description: 'All officers can host this.' },
         { name: 'Lore Operation', value: 'Lore Operation', description: 'Lore Operation.' },
         { name: 'Lore Battle', value: 'Lore Battle', description: 'Coming soon.' },
     ],
-    inspection: [
+    inspections: [
         { name: 'Wing/Bat/Ship Inspection', value: 'Wing/Bat/Ship Inspection', description: 'Inspection for sub-divisions.' },
         { name: 'Division Inspection', value: 'Division Inspection', description: 'Inspection for divisions.' },
         { name: 'Branch Inspection', value: 'Branch Inspection', description: 'Inspection for branches.' },
+        { name: 'Military-wide Inspection', value: 'Military-wide Inspection', description: 'Inspection for the military to ensure activity.' },
     ],
+    milsim: [
+        { name: 'FTX', value: 'FTX', description: 'Field Training Exercise.' },
+        { name: 'Military Exercise', value: 'Military Exercise', description: 'Milsim exercise for units in Yellonia.' },
+        { name: 'Full Mission Profile', value: 'Full Mission Profile', description: 'Simulated exercise for units in Yellonia.' },
+        { name: 'Milsim Training', value: 'Milsim Training', description: 'Title must be used to specify what area of milsim training this is about.' },
+        { name: 'Milsim Class', value: 'Milsim Class', description: 'Title must be used to specify what area of milsim class this is about.' },
+     ],
+    fun [
+        { name: 'Practice Raid', value: 'Practice Raid', description: 'Practice Raid.' },
+        { name: 'Game Night/Day', value: 'Game Night/Day', description: 'Playing a fun game of sorts.' },
+        { name: 'Game Raid', value: 'Game Raid', description: 'Raid a game with a bunch of people.' },
+        { name: 'Sword Fight', value: 'Sword Fight', description: 'Challenge others to swordfighting for fun!' },
+        { name: 'Team Deathmatch', value: 'Game Night/Day', description: 'Challenging some friends to a team deathmatch on one of our many battle games.' },
+     ],
     other: [
         { name: 'Server Start-up', value: 'Server Start-up', description: 'SSU.' },
         { name: 'Recruitment Session', value: 'Recruitment Session', description: 'Recruitment Session.' },
-        { name: 'Practice Raid', value: 'Practice Raid', description: 'Practice Raid.' },
-        { name: 'Game Night/Day', value: 'Game Night/Day', description: 'Playing a fun game of sorts.' },
-        { name: 'Game Raid', value: 'Game Raid', description: 'Raiding a game with a bunch of people.' },
-        { name: 'Battle', value: 'Battle', description: 'Battle against a different group.' },
-        { name: 'Award Ceremony', value: 'Award Ceremony', description: 'The most prestigious event in Yellonia.' },
-        { name: 'Other', value: 'Other', description: 'Include a post below stating what the event is.' },
+        { name: 'Award Ceremony', value: 'Award Ceremony', description: 'Required for giving out awards that require such ceremonies in Yellonia.'},
+        { name: 'Court Martial', value: 'Court Martial', description: 'This event is only used for when major CMs happen.' },
+        { name: 'Other', value: 'Other', description: 'Use title to show the event.' },
     ],
     },
 
